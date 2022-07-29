2a8cb22b83910b60ead5f0904c283cdce68e899a
muskansogani <91368207+muskansogani@users.noreply.github.com>
2022-07-29 21:59:42 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,87 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "Which of the following folk dance forms is associated with Gujarat?",
+    answer: "Garba",
+    options: ["Nautanki", "Garba", "Kathakali", "Bhagra"]
+  },
+  {
+    numb: 2,
+    question:
+      "Rabindranath Tagore's 'Jana Gana Mana' has been adopted as India's National Anthem. How many stanzas of the said song were adopted?",
+    answer: "Only the first stanza",
+    options: [
+      "Only the first stanza",
+      "The whole song",
+      "Third and fourth stanza",
+      "First and second stanza"
+    ]
+  },
+  {
+    numb: 3,
+    question: "The National Song of India was composed by?",
+    answer: "Bankim Chandra Chatterji",
+    options: [
+      "Rabindranath Tagore",
+      "Iqbal",
+      "Bankim Chandra Chatterji",
+      "Jai Shankar Prasad"
+    ]
+  },
+  {
+    numb: 4,
+    question:
+      "In which of the following festivals are boat races a special feature?",
+    answer: "Onam",
+    options: ["Pongal", "Bihu", "Navratri", "Onam"]
+  },
+  {
+    numb: 5,
+    question:
+      "Which of the following places is famous for Chikankari work, which is a traditional art of embroidery?",
+    answer: "Lucknow",
+    options: ["Jaipur", "Hyderabad", "Lucknow", "Mysore"]
+  },
+  {
+    numb: 6,
+    question: "'Kathakali' is a folk dance prevalent in which state?",
+    answer: "Kerala",
+    options: ["Orissa", "Kerala", "Karnataka", "Manipur"]
+  },
+  {
+    numb: 7,
+    question: "India's largest city by population?",
+    answer: "Mumbai",
+    options: ["Mumbai", "Kerala", "Chennai", "Delhi"]
+  },
+  {
+    numb: 8,
+    question: "Number of states and Union Territories in India?",
+    answer: "28 states and 8 Union territories",
+    options: [
+      "29 states and 8 Union territories",
+      "28 states and 7 Union territories",
+      "28 states and 8 Union territories",
+      "29 states and 8 Union territories"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Hindi Divas is celebrated in India on?",
+    answer: "14th September",
+    options: [
+      "19th December",
+      "14th September",
+      "14th November",
+      "15th January"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Which is the largest coffee-producing state of India?",
+    answer: "Karnataka",
+    options: ["Orissa", "Kerala", "Karnataka", "Tamil Nadu"]
+  }
+];
